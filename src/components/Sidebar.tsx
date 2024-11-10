6f93faa07d682ac153f4302b8f891b997ebce354
abdul <likeam99@gmail.com>
2024-11-10 08:11:45 +0500

    Building Initial Setup

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Sidebar = () => {
+  const [categories, setCategories] = useState<string[]>([]);
+  const [keywords] = useState<string[]>([
+    "apple",
+    "watch",
+    "Fashion",
+    "trend",
+    "shoes",
+    "shirt",
+  ]);
+
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
